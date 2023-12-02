438ff6fc6a26aca8ecc99b25a3caf78d446a40c9
soray677 <soray677@outlook.com>
2023-12-03 05:24:30 +0900

    関数仮置き

---
+++ src/submiter.py
@@ -0,0 +1,17 @@
+#
+# 解提出管理
+#
+
+SOLVE_SINGLE_ID = 's' # 単目的
+SOLVE_MULTI_ID  = 'm' # 多目的 
+
+def submit(dep, num, ans):
+    """解提出
+
+    Args:
+        dep (string): 問題部門
+        num (int): 問題番号
+        ans (any): 解
+    """
+    # Todo: 解答提出スクリプト
+    pass
