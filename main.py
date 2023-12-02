a849ace14d74a9d33b73645596a18759e646f5bb
soray677 <soray677@outlook.com>
2023-12-03 05:24:42 +0900

    変数置き換え

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import click
 import src.process as process
-import src.util.logger as logger
+import src.submiter as submiter
 
 @click.command()
-@click.option('--dep', prompt='部門選択(`s`:単目的|`m`:多目的)', type=click.Choice(['s', 'm'], case_sensitive=False))
+@click.option('--dep', prompt=f'部門選択(`{submiter.SOLVE_SINGLE_ID}`:単目的|{submiter.SOLVE_MULTI_ID}:多目的)', type=click.Choice([submiter.SOLVE_SINGLE_ID, submiter.SOLVE_MULTI_ID], case_sensitive=False))
 @click.option('--num', help='番号選択(`0`:練習|`1`以上:本番)', type=int, default=0)
 def main(dep, num):
     """メイン処理
@@ -15,6 +15,7 @@ def main(dep, num):
     """
     process.init(os.getcwd())
     process.run(dep, num)
+    process.terminate()
 
 if __name__ == '__main__':
     main()
