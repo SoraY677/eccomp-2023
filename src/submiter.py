cb2cbd776578f8c4c88737b789f04ca445407cde
soray677 <soray677@outlook.com>
2023-12-03 06:47:02 +0900

    形式を変更

--- src/submiter.py
+++ src/submiter.py
@@ -7,13 +7,30 @@ from util import logger
 SOLVE_SINGLE_ID = 's' # 単目的
 SOLVE_MULTI_ID  = 'm' # 多目的
 
-# 実際の提出時に使用する問題指定用番号
-MATCH_NUM = {
-    f"{SOLVE_SINGLE_ID}0": 93, # https://ec-comp.jpnsec.org/ja/matches/93
-    f"{SOLVE_SINGLE_ID}1": 94, # https://ec-comp.jpnsec.org/ja/matches/94
-    f"{SOLVE_SINGLE_ID}2": 95, # https://ec-comp.jpnsec.org/ja/matches/95
-    f"{SOLVE_MULTI_ID}0" : 96, # https://ec-comp.jpnsec.org/ja/matches/96
-    f"{SOLVE_MULTI_ID}1" : 97, # https://ec-comp.jpnsec.org/ja/matches/97
+
+QUESTION_MAP = {
+    ### 単目的
+    # https://ec-comp.jpnsec.org/ja/matches/93
+    f"{SOLVE_SINGLE_ID}0": {
+        "match_num": 93
+    },
+    # https://ec-comp.jpnsec.org/ja/matches/94
+    f"{SOLVE_SINGLE_ID}1": {
+        "match_num": 94,
+    },
+    # https://ec-comp.jpnsec.org/ja/matches/95
+    f"{SOLVE_SINGLE_ID}2": {
+        "match_num": 95
+    },
+    ### 多目的
+    # https://ec-comp.jpnsec.org/ja/matches/96
+    f"{SOLVE_MULTI_ID}0": {
+        "match_num": 96
+    },
+    # https://ec-comp.jpnsec.org/ja/matches/97
+    f"{SOLVE_MULTI_ID}1": {
+        "match_num": 97
+    }
 }
 
 def get_match_num(dep, num):
@@ -24,8 +41,8 @@ def get_match_num(dep, num):
         num (int): 問題番号
     """
     key = f"{dep}{num}"
-    if key in MATCH_NUM:
-        match_num = MATCH_NUM[f"{dep}{num}"]
+    if key in QUESTION_MAP:
+        match_num = QUESTION_MAP[f"{dep}{num}"]["match_num"]
     else:
         logger.error(f"問題のID`{dep}{num}`は存在しない")
         sys.exit(1)
