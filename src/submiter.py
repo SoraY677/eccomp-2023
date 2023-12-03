8d8a224b707c84641b4b6fce1f78a392a66264e4
soray677 <soray677@outlook.com>
2023-12-03 10:04:16 +0900

    提出部を仮作成

--- src/submiter.py
+++ src/submiter.py
@@ -2,6 +2,8 @@
 # 解提出管理
 #
 import sys
+import subprocess
+import json
 from util import logger
 
 SOLVE_SINGLE_ID = 's' # 単目的
@@ -37,7 +39,7 @@ QUESTION_MAP = {
     }
 }
 
-def get_match_num(dep, num):
+def _get_match_num(dep, num):
     """問題指定用番号を取得
 
     Args:
@@ -72,6 +74,43 @@ def get_submit_max(dep, num):
         sys.exit(1)
     return submit_max
 
+def _decode_response(response_txt):
+    """レスポンスのデコード
+
+    Args:
+        response_txt (string): レスポンス内容文字列 
+
+    Returns:
+        dict: レスポンス内容の辞書型
+    """
+    decoded_response = json.loads(response_txt)
+    logger.info(f'レスポンス: {decoded_response}')
+    return decoded_response
+
+def _exec_submit_command(dep, num, ans):
+    """実行コマンド
+
+    Args:
+        dep (string): 問題部門
+        num (int): 問題番号
+        ans (dict): 解答
+
+    Returns:
+        dict: 成功:レスポンス|失敗:カラ辞書型配列
+    """
+    match_num = _get_match_num(dep, num)
+    
+    command = f'echo \'{ans}\' | opt submit --match={match_num}'
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+    try:
+        response = _decode_response(proc.communicate()[0])
+    except Exception as e:
+        logger.warn(e)
+        response = {}
+
+    return response
+
 def submit(dep, num, ans):
     """解提出
 
@@ -80,8 +119,8 @@ def submit(dep, num, ans):
         num (int): 問題番号
         ans (any): 解
     """
-    # Todo: 解答提出スクリプト
-    pass
+    # Todo:テストできていないので、テストコードなど修正
+    return _exec_submit_command(dep, num, ans)
 
 # 
 # 単体テスト
@@ -92,7 +131,7 @@ if __name__ == "__main__":
         def test_get_match_num(self):
             """問題指定用番号の取得
             """
-            match_num = get_match_num('s', 1)
+            match_num = _get_match_num('s', 1)
             self.assertTrue(match_num == 94)
             
         def test_get_match_num_error(self):
@@ -100,7 +139,7 @@ if __name__ == "__main__":
             """
             logger.init()
             with self.assertRaises(SystemExit):
-                get_match_num('n', 1)
+                _get_match_num('n', 1)
                 
         def test_get_submit_max(self):
             """提出回数の取得
@@ -114,5 +153,5 @@ if __name__ == "__main__":
             logger.init()
             with self.assertRaises(SystemExit):
                 get_submit_max('n', 1)
-        
+
     unittest.main()
