4c9cf27cd32c1b6c7fafea8d6cf99129ffa90ced
soray677 <soray677@outlook.com>
2023-12-10 00:31:17 +0900

    コメントを追加

--- src/solution/individual.py
+++ src/solution/individual.py
@@ -11,19 +11,43 @@ class Individual:
     _content = {
         INDIVIDUAL_CONTENT_SCHEDULE_KEY: []
     }
-    def __init__(self, work_num):
-        self._content[INDIVIDUAL_CONTENT_SCHEDULE_KEY] = self._create(work_num)
+    def __init__(self, work_num, ban_generation_list):
+        """初期化
+
+        Args:
+            work_num (int): ワーク数
+            ban_generation_list (list): 作成禁止リスト（過去に作成済み）
+        """
+        self._content[INDIVIDUAL_CONTENT_SCHEDULE_KEY] = self._create(work_num, ban_generation_list)
     
-    def _create(self, work_num):
+    def _create(self, work_num, ban_generation_list = None):
+        """生成
+
+        Args:
+            work_num (int): ワーク数
+            ban_generation_list (list): 作成禁止リスト（過去に作成済み）
+
+        Returns:
+            list: 開始時間・終了時間の配列
+        """
         result = []
-        for _ in range(work_num):
-            start = random.randint(WORK_MIN_DAY, WORK_MAX_DAY)
-            end = random.randint(start, WORK_MAX_DAY)
-            result.append(start)
-            result.append(end)
+        while(True):
+            for _ in range(work_num):
+                start = random.randint(WORK_MIN_DAY, WORK_MAX_DAY)
+                end = random.randint(start, WORK_MAX_DAY)
+                result.append(start)
+                result.append(end)
+            if result not in ban_generation_list:
+                break
+        
         return result
     
     def get_schedule(self):
+        """スケジュール要素を取得
+
+        Returns:
+            str: スケジュールの配列
+        """
         return self._content[INDIVIDUAL_CONTENT_SCHEDULE_KEY]
     
 #
