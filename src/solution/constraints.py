bde234880dec9849a1435a7ebe37176aac451eb9
soray677 <soray677@outlook.com>
2023-12-10 00:14:21 +0900

    定数を定義

---
+++ src/solution/constraints.py
@@ -0,0 +1,20 @@
+#
+# 定数
+#
+
+# 制約条件関連
+## ワーク数の最大最小
+WORK_MIN_DAY = 1
+WORK_MAX_DAY = 9
+
+# 初期生成する解の最大数
+INITIALIZE_INDIVIDUAL_MAX = 1000
+
+# 進化計算アルゴリズム関連
+INDIVISUAL_MAX = 20 # 解の個数
+
+
+# クラスタ関連
+CLUSTER_MAX_DEFAULT = 20 # クラスタリング数
+CLUSTER_LOOP_MAX_DEFAULT = 1000 # 重心算出回数の上限
+
