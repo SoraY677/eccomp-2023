3cd10fac979d12ffcaf61c1eec41316443414714
soray677 <soray677@outlook.com>
2023-12-16 13:41:02 +0900

    plot用変換処理追加

--- src/solution/individual.py
+++ src/solution/individual.py
@@ -10,6 +10,8 @@ else:
 
 INDIVIDUAL_CONTENT_SCHEDULE_KEY = "schedule"
 INDIVIDUAL_CONTENT_WEIGHTS_KEY = "weights"
+INDIVIDUAL_PLOT_LIST_X_KEY = "x"
+INDIVIDUAL_PLOT_LIST_Y_KEY = "y"
 
 class Individual:
     _content = {}
@@ -77,7 +79,7 @@ class Individual:
         """
         return [random.random() for _ in range(weight_num)] 
 
-    def get_schedule(self):
+    def get_schedule_list(self):
         """スケジュール要素を取得
 
         Returns:
@@ -92,6 +94,30 @@ class Individual:
             float[]: SCIP重みの配列
         """
         return self._content[INDIVIDUAL_CONTENT_WEIGHTS_KEY]
+    
+    def get_plot_list(self):
+        result = []
+        # スケジュール
+        # x -> 工数 = endtime - starttime
+        # y -> 優先度 = starttime
+        schedule_list= self.get_schedule_list()
+        if len(schedule_list) != 0:
+            result.extend([{
+                INDIVIDUAL_PLOT_LIST_X_KEY: schedule_list[i+1] - schedule_list[i],
+                INDIVIDUAL_PLOT_LIST_Y_KEY: schedule_list[i+1],
+            } for i in range(0, len(schedule_list), 2)])
+        # SCIP重み
+        # x,y -> 異なる重み
+        weight_list = self.get_weight_list()
+        if len(weight_list) != 0:
+            for line_i in range(1, len(weight_list), 1):
+                for row_i in range(line_i):
+                    result.append({
+                        INDIVIDUAL_PLOT_LIST_X_KEY: weight_list[row_i],
+                        INDIVIDUAL_PLOT_LIST_Y_KEY: weight_list[line_i],
+                    })
+
+        return result
     
     def serialize(self):
         """シリアライズ
@@ -125,13 +151,13 @@ if __name__ == "__main__":
             work_num = 10
             weight_num = 4
             individual = Individual(work_num=work_num, weight_num=weight_num)
-            self.assertTrue(len(individual.get_schedule()) == work_num * 2)
+            self.assertTrue(len(individual.get_schedule_list()) == work_num * 2)
             self.assertTrue(len(individual.get_weight_list()) == weight_num)
         def test_designation_create_individual(self):
             schedule_list = [7, 7, 6, 8, 3, 7, 9, 9, 6, 9, 2, 2, 1, 8, 1, 2, 1, 7, 7, 8]
             weight_list = [0.6947156931851072, 0.5260175487956781, 0.7449466428168457, 0.29364899784938414]
             individual = Individual(schedule_list=schedule_list, weight_list=weight_list)
-            self.assertTrue(individual.get_schedule() == schedule_list)
+            self.assertTrue(individual.get_schedule_list() == schedule_list)
             self.assertTrue(individual.get_weight_list() == weight_list)
         def test_serialize(self):
             schedule_list = [7, 7, 6, 8, 3, 7, 9, 9, 6, 9, 2, 2, 1, 8, 1, 2, 1, 7, 7, 8]
@@ -150,8 +176,14 @@ if __name__ == "__main__":
                     INDIVIDUAL_CONTENT_SCHEDULE_KEY: schedule_list,
                     INDIVIDUAL_CONTENT_WEIGHTS_KEY: weight_list
             })
-            self.assertTrue(individual.get_schedule() == schedule_list)
+            self.assertTrue(individual.get_schedule_list() == schedule_list)
             self.assertTrue(individual.get_weight_list() == weight_list)
+        def test_get_plot_list(self):
+            schedule_list = [7, 7, 6, 8, 3, 7, 9, 9, 6, 9, 2, 2, 1, 8, 1, 2, 1, 7, 7, 8]
+            weight_list = [0.6947156931851072, 0.5260175487956781, 0.7449466428168457, 0.29364899784938414]
+            individual = Individual(schedule_list=schedule_list, weight_list=weight_list)
+            plot_list = individual.get_plot_list()
+            self.assertTrue(len(plot_list) == 16)
         # Todo: 禁止リストに関するテスト
             
     unittest.main()
